181cf160473c6e9152fbe69fb1f670af93e12da9
Glitch (cloud-rune-agreement) <none>
2020-11-23 19:01:11 +0000

    🎆🐗 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# bot
+# Welcome to my Dropify GLITCH.
+
+
+## You probaly have been invited here for restocks...
+
+---
+
+Hello! How to restock:
+Go to the folder accounts/ and open (accounts/disney.txt) boom you can add accounts there!!!
+
+---
+
+Heres some important links for later refernce
+
+https://glitch.com/edit/#!/starter-discord?path=server.js%3A8%3A0
+https://uptimerobot.com/dashboard.php#mySettings
+
+Here's some helpful links:
+
+Account Checkers: 
+https://www.sayapro.us/acc/ (money grabber)
+https://checkz.net (free no sign ups)
+Account Gens:
+https://https://iraven.cc (unchecked 10 times per day most don't work)
+https://h-gen.xyz (working)
---
+++ commands/proxy.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const cooldown = new Set();
+
+module.exports.run = async (client, msg, args, config) => {
+    if(cooldown.has(msg.author.id)) {
+        msg.reply(`:alarm_clock: Vous pourrez regénérer dans ${config.COOLDOWN} minutes.`)
+            .then((m) => {
+                msg.delete();
+
+                setTimeout(() => {
+                    m.delete();
+                }, 5000);
+            });
+    } else {
+        fs.readFile('./accounts/proxy.txt', 'utf8', function(err, data) {
+            if (err) throw err;
+
+            data = data + '';
+            var lines = data.split('\n');
+            let account = lines[Math.floor(Math.random() * 1)];
+
+            fs.writeFile('./accounts/proxy.txt', lines.slice(1).join('\n'), function(err) {
+                if(err) throw err;
+            });
+
+            let embed = new Discord.RichEmbed()
+            .setTitle('Voici votre nouveau Proxy !')
+            .setDescription(`\`\`\`${account} | Gen by NightGen\`\`\` \nSouvenez-vous que vous utilisez un générateur gratuit et sans pub, donc si un compte ne fonctionne pas veuillez en regénérer un autre.`)
+            .setThumbnail('')
+            .setImage('')
+            .setColor("#363940")
+            .setFooter('NightGen')
+            .setTimestamp();
+
+            msg.author.send(embed);
+
+            msg.reply(':white_check_mark: Votre Proxy vient d\'etre généré en MP!')
+                .then(m => {
+                    setTimeout(() => {
+                        m.delete();
+                    }, 900000);
+                });
+
+            cooldown.add(msg.author.id);
+            setTimeout(() => {
+                cooldown.delete(msg.author.id);
+            }, config.COOLDOWN * 60 * 1000);
+        });
+    }
+};
+
+module.exports.help = {
+    name: `proxy`,
+    description: `Sends you a proxy https`
+};
